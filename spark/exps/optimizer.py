0e254eaf7a4877e374140217035ca552fabcced4
Supun Nakandala <supun.nakandala@gmail.com>
2019-03-07 20:18:36 +0000

    wip file

--- spark/exps/optimizer.py
+++ spark/exps/optimizer.py
@@ -24,23 +24,77 @@ import time
 if __name__ == '__main__':
 
     def downstream_ml_func(features_df, results_dict, layer_index):
-        lr = LogisticRegression(labelCol="label", featuresCol="features", maxIter=10, regParam=0.5)
-        model = lr.fit(features_df)
-        predictions = model.transform(features_df)
+        lr = LogisticRegression(labelCol="label", featuresCol="features", elasticNetParam=0.5, regParam=0.01)
+        train_df, test_df = features_df.randomSplit([0.8, 0.2], seed=2019)
+        #valid_df, test_df = test_df.randomSplit([0.5, 0.5], seed=2018)
+
+        model = lr.fit(train_df)
+        result = {}
+
+        predictions = model.transform(train_df)
         evaluator = MulticlassClassificationEvaluator(labelCol="label", predictionCol="prediction",
-                                                      metricName="accuracy")
-        results_dict[layer_index] = evaluator.evaluate(predictions)
+                                                      metricName="f1")
+        result['train_acc'] = evaluator.evaluate(predictions)
+
+        #predictions = model.transform(valid_df)
+        #evaluator = MulticlassClassificationEvaluator(labelCol="label", predictionCol="prediction",
+        #                                             metricName="f1")
+        #result['valid_acc'] = evaluator.evaluate(predictions)
+
+        predictions = model.transform(test_df)
+        evaluator = MulticlassClassificationEvaluator(labelCol="label", predictionCol="prediction",
+                                                      metricName="f1")
+        result['test_acc'] = evaluator.evaluate(predictions)
+
+        results_dict[layer_index] = result
         return results_dict
+    
+#     vista = Vista("vista-example", 16, 8, 8, 'alexnet', 4, 0, downstream_ml_func, 'hdfs://spark-cluster-master:9000/amazon.csv', 'hdfs://spark-cluster-master:9000/amazon_images', 20000, 100, mem_sys_rsv=3)
 
-    prev_time = time.time()
-    # mem_sys_rsv is an optional parameter. If not set a default value of 3 will be used.
-    vista = Vista("vista-example", 32, 8, 8, 'alexnet', 4, 0, downstream_ml_func, 'hdfs://spark-cluster-master:9000/foods.csv',
-                      'hdfs://spark-cluster-master:9000/images', 20129, 130, mem_sys_rsv=3)
+#     result = vista.run()
+#     for k in [-1, -2, -3, -4]:
+#         print(k, result[k])
 
-    # Optional overrides
-    vista.override_inference_type('bulk')
-    vista.override_join('s')
-    vista.overrdide_operator_placement('before-join')
+    
+    import pyspark
+    from pyspark.ml.feature import VectorAssembler
+    from pyspark.sql import functions as F
+    from pyspark.ml.linalg import Vectors, VectorUDT
+    from pyspark.sql.types import StringType, FloatType, IntegerType
+    
+    toDense = F.udf(lambda v: Vectors.dense(v.toArray()), VectorUDT())
+    
+    sc = pyspark.SparkContext(appName="vista")
+    
+    
+    def get_struct_df(sc, data_file_path):
+        sql_context = pyspark.SQLContext(sc)
+        struct_df = sql_context.read.format('csv').options(header='false', inferSchema='true').load(data_file_path)
+        col_names = struct_df.schema.names
 
-    print(vista.run())
-    print("Runtime: " + str((time.time()-prev_time))/60.0)
+        struct_df = VectorAssembler(inputCols=col_names[1:-1], outputCol="features").transform(struct_df)
+        struct_df = struct_df.withColumn("features", toDense("features"))
+        struct_df = struct_df.withColumn("id", struct_df[col_names[0]].cast(StringType())) \
+            .withColumn("label", struct_df[col_names[-1]].cast(IntegerType())) \
+            .select("id", "features", "label")
+        return struct_df
+    
+    
+    
+#     features_df = get_struct_df(sc, 'hdfs://spark-cluster-master:9000/amazon.csv')
+#     print(downstream_ml_func(features_df, {}, 0))
+
+    def get_hog_df(sc, data_file_path):
+        sql_context = pyspark.SQLContext(sc)
+        struct_df = sql_context.read.format('csv').options(header='false', inferSchema='true').load(data_file_path)
+
+        col_names = struct_df.schema.names
+
+        struct_df = VectorAssembler(inputCols=col_names[:-1], outputCol="features").transform(struct_df)
+        struct_df = struct_df.withColumn("features", toDense("features"))
+        struct_df = struct_df.withColumn("label", struct_df[col_names[-1]].cast(IntegerType())) \
+            .select("features", "label")
+        return struct_df
+    
+    hog_df = get_hog_df(sc, 'hdfs://spark-cluster-master:9000/amazon_with_hog.csv')
+    print(downstream_ml_func(hog_df, {}, 0))
